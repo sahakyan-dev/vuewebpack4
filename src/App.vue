ff2da05652a2a3ba7fc9897183892fc64ed61a8e
Norayr <baghdasarov94@gmail.com>
2019-03-01 01:19:51 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="full-width center-content">
+    <home-component name="World" />
+  </div>
+</template>
+<script>
+import HomeComponent from './components/HomeComponent.vue'
+export default {
+  components: {
+    HomeComponent
+  }
+}
+</script>
